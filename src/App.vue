55c528384ce2067a0c841f71ea37a6048f4e3101
Kristijan Jurkovic <kristijanjurkovic@KristijansMBP2.fritz.box>
2022-03-15 18:05:10 +0000

    instacard v-for with props

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+          <img src="@/assets/logo.png" alt="" height="32" class="d-inline-block align-text-top" loading="lazy"/>
+        </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link">Prijava</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/signup" class="nav-link">Registracija</router-link>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Pretraga" aria-label="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+    <router-view/>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="row">
+        <div class="col-8">
+            <instagram-card v-for="card in cards" :key="card" :info="card"/>
+        </div>
+        <div class="col-4">
+            <p v-for="card in cards" :key="card">
+                Ovo je text ... {{ card }}
+            </p>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import InstagramCard from '@/components/InstagramCard.vue';
+
+let cards = [
+  "https://picsum.photos/id/1/400/400",
+  "https://picsum.photos/id/2/400/400",
+  "https://picsum.photos/id/3/400/400",
+  "https://picsum.photos/id/4/400/400",
+]
 
 export default {
   name: 'HomeView',
+  data: () => {
+    return {
+      cards: cards
+    }
+  },
   components: {
-    HelloWorld
+    InstagramCard,
   }
-}
+};
 </script>
